9bc639f57def5c9d5b4a1c28d43b1ae92407cf0b
Filip Markovic <107422386+FilipM79@users.noreply.github.com>
2022-11-13 22:07:37 +0100

    Basic calculator - stage 2/4 not finished but working

--- Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
+++ Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
@@ -1,56 +1,55 @@
 package org.hyperskill.calculator
 
 import android.os.Bundle
-import android.text.Editable
-import android.view.View
-import android.view.View.OnClickListener
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     
     private lateinit var calcDisplay: EditText
-    private var x by Delegates.notNull<Double>()
-    private var operand by Delegates.notNull<String>()
+    private var x: Double = 0.0
+    private var operand = ""
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     
         calcDisplay = findViewById(R.id.editText)
-        val zero: Button = setupButton(R.id.button0)
-        val one: Button = setupButton(R.id.button1)
-        val two: Button = setupButton(R.id.button2)
-        val three: Button = setupButton(R.id.button3)
-        val four: Button = setupButton(R.id.button4)
-        val five: Button = setupButton(R.id.button5)
-        val six: Button = setupButton(R.id.button6)
-        val seven: Button = setupButton(R.id.button7)
-        val eight: Button = setupButton(R.id.button8)
-        val nine: Button = setupButton(R.id.button9)
         
-        val dot: Button = findViewById(R.id.dotButton)
+        if (!calcDisplay.text.equals("0")) setupButton(R.id.button0)
+        setupButton(R.id.button1)
+        setupButton(R.id.button2)
+        setupButton(R.id.button3)
+        setupButton(R.id.button4)
+        setupButton(R.id.button5)
+        setupButton(R.id.button6)
+        setupButton(R.id.button7)
+        setupButton(R.id.button8)
+        setupButton(R.id.button9)
+        
         val clear : Button= findViewById(R.id.clearButton)
+        val dot: Button = findViewById(R.id.dotButton)
         val equal: Button = findViewById(R.id.equalButton)
+        
         val minus: Button = findViewById(R.id.subtractButton)
         val plus: Button = findViewById(R.id.addButton)
         val multiply: Button = findViewById(R.id.multiplyButton)
         val divide: Button = findViewById(R.id.divideButton)
-        
-        clear.setOnClickListener { calcDisplay.text.clear() }
+    
+        clear.setOnClickListener {
+            calcDisplay.text.clear()
+            calcDisplay.hint = "0"
+        }
+    
         dot.setOnClickListener { calcDisplay.text.append(".")  }
-        
-        minus.performOperation(R.id.subtractButton)
-        plus.performOperation(R.id.addButton)
-        multiply.performOperation(R.id.multiplyButton)
-        divide.performOperation(R.id.divideButton)
         
         equal.setOnClickListener {
             val y = calcDisplay.text.toString().toDouble()
             val result: Double
+            Log.i(x.toString(), "iz equal x: $x")
     
             if (y.equals(0.0)) {
                 calcDisplay.text.clear()
@@ -61,19 +60,31 @@ class MainActivity : AppCompatActivity() {
                     "-" -> x - y
                     "+" -> x + y
                     "*" -> x * y
-                    else -> x / y
+                    "/" -> x / y
+                    else -> 0.01
                 }
+                Log.i(result.toString(), "result form equal: $result")
                 calcDisplay.text.append(result.toString())
             }
         }
+        
+        performOperation(minus)
+        performOperation(plus)
+        performOperation(multiply)
+        performOperation(divide)
     }
     
-    private fun Button.performOperation(@IdRes resId: Int): Button =
-        findViewById<Button>(resId).apply {
-            this.setOnClickListener {
-                x = calcDisplay.text.toString().toFloat()
-                operand = text.toString()
-                calcDisplay.text.clear()
+    private fun performOperation(button: Button) {
+        button.setOnClickListener {
+            x = if (calcDisplay.text.isEmpty()) {
+                0.0
+            } else {
+                calcDisplay.text.toString().toDouble()
+            }
+            Log.i(x.toString(), "iz perform operation x: $x")
+            operand = button.text.toString()
+            calcDisplay.text.clear()
+            calcDisplay.hint = String.format("%s %s", x.toString(), operand)
         }
     }
     
